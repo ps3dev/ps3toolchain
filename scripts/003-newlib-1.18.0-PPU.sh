52f12c44b69abb779df2e9f781a8594339b57c53
Dan Peori <dan.peori@oopo.net>
2010-11-14 03:23:49 -0400

    Added some stub functions to newlib for the ppu.

--- scripts/003-newlib-1.18.0-PPU.sh
+++ scripts/003-newlib-1.18.0-PPU.sh
@@ -14,11 +14,7 @@ cat ../../patches/newlib-1.18.0-PPU.patch | patch -p1 || { exit 1; }
 mkdir build-ppu && cd build-ppu || { exit 1; }
 
 ## Configure the build.
-../configure --prefix="$PS3DEV/ppu" --target="ppu" \
-    --disable-multilib \
-    --disable-nls \
-    --disable-shared \
-    || { exit 1; }
+../configure --prefix="$PS3DEV/ppu" --target="ppu" --disable-multilib --disable-nls --disable-shared || { exit 1; }
 
 ## Compile and install.
 make -j 4 && make install || { exit 1; }
