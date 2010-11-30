57907a3cf7a1f952f88d7bf584a82829ecde8674
Dan Peori <dan.peori@oopo.net>
2010-11-30 14:42:16 -0400

    Updated to binutils-2.20.1.

--- scripts/001-binutils-2.20.1-PPU.sh
+++ scripts/001-binutils-2.20.1-PPU.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-# binutils-2.20-PPU.sh by Dan Peori (dan.peori@oopo.net)
+# binutils-2.20.1-PPU.sh by Dan Peori (dan.peori@oopo.net)
 
 ## Download the source code.
-wget --continue ftp://ftp.gnu.org/gnu/binutils/binutils-2.20.tar.bz2 || { exit 1; }
+wget --continue ftp://ftp.gnu.org/gnu/binutils/binutils-2.20.1.tar.bz2 || { exit 1; }
 
 ## Unpack the source code.
-rm -Rf binutils-2.20 && tar xfvj binutils-2.20.tar.bz2 && cd binutils-2.20 || { exit 1; }
+rm -Rf binutils-2.20.1 && tar xfvj binutils-2.20.1.tar.bz2 && cd binutils-2.20.1 || { exit 1; }
 
 ## Patch the source code.
-cat ../../patches/binutils-2.20-PPU.patch | patch -p1 || { exit ; }
+cat ../../patches/binutils-2.20.1-PPU.patch | patch -p1 || { exit ; }
 
 ## Create the build directory.
 mkdir build-ppu && cd build-ppu || { exit 1; }
--- scripts/006-binutils-2.20.1-SPU.sh
+++ scripts/006-binutils-2.20.1-SPU.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# binutils-2.20-SPU.sh by Dan Peori (dan.peori@oopo.net)
+# binutils-2.20.1-SPU.sh by Dan Peori (dan.peori@oopo.net)
 
 ## Download the source code.
-wget --continue ftp://ftp.gnu.org/gnu/binutils/binutils-2.20.tar.bz2 || { exit 1; }
+wget --continue ftp://ftp.gnu.org/gnu/binutils/binutils-2.20.1.tar.bz2 || { exit 1; }
 
 ## Unpack the source code.
-rm -Rf binutils-2.20 && tar xfvj binutils-2.20.tar.bz2 && cd binutils-2.20 || { exit 1; }
+rm -Rf binutils-2.20.1 && tar xfvj binutils-2.20.1.tar.bz2 && cd binutils-2.20.1 || { exit 1; }
 
 ## Create the build directory.
 mkdir build-spu && cd build-spu || { exit 1; }
