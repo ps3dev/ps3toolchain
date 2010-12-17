04aff4966f8a67000481f2bffa036c964d037790
Dan Peori <dan.peori@oopo.net>
2010-12-17 12:41:23 -0400

    Updated newlib patch. Fixed ps3dev depend check script.

--- depends/check-ps3dev.sh
+++ depends/check-ps3dev.sh
@@ -14,7 +14,7 @@ touch $PS3DEV/test.tmp 1> /dev/null || { echo "ERROR: Grant write permissions fo
 echo $PATH | grep $PS3DEV/bin 1> /dev/null || { echo "ERROR: Add $PS3DEV/bin to your path before continuing."; exit 1; }
 
 ## Check for $PS3DEV/ppu/bin in the path.
-echo $PATH | grep $PS3DEV/ppu/bin 1> /dev/null || { echo "ERROR: Add $PS3DEV/ppu/bin to your path before continuing."; exit 1; }
+echo $PATH | grep $PS3DEV/host/ppu/bin 1> /dev/null || { echo "ERROR: Add $PS3DEV/host/ppu/bin to your path before continuing."; exit 1; }
 
 ## Check for $PS3DEV/spu/bin in the path.
-echo $PATH | grep $PS3DEV/spu/bin 1> /dev/null || { echo "ERROR: Add $PS3DEV/spu/bin to your path before continuing."; exit 1; }
+echo $PATH | grep $PS3DEV/host/spu/bin 1> /dev/null || { echo "ERROR: Add $PS3DEV/host/spu/bin to your path before continuing."; exit 1; }
