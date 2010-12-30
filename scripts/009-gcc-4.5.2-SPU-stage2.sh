126e3437ec0dfd8202b04cd229e2c46815dc5fec
Dan Peori <dan.peori@oopo.net>
2010-12-29 20:56:09 -0400

    Updated to gcc-4.5.2.

---
+++ scripts/009-gcc-4.5.2-SPU-stage2.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# gcc-4.5.2-SPU-stage2.sh by Dan Peori (dan.peori@oopo.net)
+
+## Download the source code.
+wget --continue ftp://ftp.gnu.org/gnu/gcc/gcc-4.5.2/gcc-4.5.2.tar.bz2 || { exit 1; }
+wget --continue ftp://ftp.gmplib.org/pub/gmp-5.0.1/gmp-5.0.1.tar.bz2 || { exit 1; }
+wget --continue http://www.multiprecision.org/mpc/download/mpc-0.8.2.tar.gz || { exit 1; }
+wget --continue http://ftp.gnu.org/gnu/mpfr/mpfr-2.4.2.tar.bz2 || { exit 1; }
+
+## Unpack the source code.
+rm -Rf gcc-4.5.2 && tar xfvj gcc-4.5.2.tar.bz2 && cd gcc-4.5.2 || { exit 1; }
+
+## Unpack the libraries.
+tar xfvj ../gmp-5.0.1.tar.bz2 && ln -s gmp-5.0.1 gmp || { exit 1; }
+tar xfvz ../mpc-0.8.2.tar.gz && ln -s mpc-0.8.2 mpc || { exit 1; }
+tar xfvj ../mpfr-2.4.2.tar.bz2 && ln -s mpfr-2.4.2 mpfr || { exit 1; }
+
+## Create the build directory.
+mkdir build-spu-stage2 && cd build-spu-stage2 || { exit 1; }
+
+## Configure the build.
+../configure --prefix="$PS3DEV/spu" --target="spu" --disable-libssp --disable-nls --enable-languages="c,c++" --with-newlib || { exit 1; }
+
+## Compile and install.
+make -j 4 all && make install || { exit 1; }
