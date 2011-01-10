2a248ac1bcc3b8d994f460b5109f29dbdc62705c
Dan Peori <dan.peori@oopo.net>
2011-01-10 10:35:25 -0400

    Addded ps3libraries to the standard install.

---
+++ scripts/012-ps3libraries.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# ps3libraries.sh by Dan Peori (dan.peori@oopo.net)
+
+## Download the source code.
+wget --continue --no-check-certificate https://github.com/ooPo/PS3LIBRARIES/tarball/master -O ps3libraries.tar.gz || { exit 1; }
+
+## Unpack the source code.
+rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries || { exit 1; }
+
+## Compile and install.
+./libraries.sh || { exit 1; }
