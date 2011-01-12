28a3f9df8cff2a106b82d6e0b1a4d5a73b366319
Dan Peori <dan.peori@oopo.net>
2011-01-12 12:37:34 -0400

    Added a check for libelf under OSX.

--- depends/check-libelf.sh
+++ depends/check-libelf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # check-libelf.sh by Dan Peori (dan.peori@oopo.net)
 
-if [ ! -f /usr/include/libelf.h ]; then { echo "ERROR: Install libelf before continuing."; exit 1; } fi
+( ls /usr/include/libelf.h || ls /opt/local/include/libelf.h ) 1> /dev/null 2> /dev/null || { echo "ERROR: Install libelf before continuing."; exit 1; }
