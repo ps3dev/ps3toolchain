25e75426c1b9f4be080a30bcbd4759c72e4b23ca
Dan Peori <dan.peori@oopo.net>
2011-01-14 17:41:11 -0400

    Updated to newlib-1.19.0.

--- scripts/008-newlib-1.19.0-SPU.sh
+++ scripts/008-newlib-1.19.0-SPU.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-# newlib-1.18.0-SPU.sh by Dan Peori (dan.peori@oopo.net)
+# newlib-1.19.0-SPU.sh by Dan Peori (dan.peori@oopo.net)
 
 ## Download the source code.
-wget --continue ftp://sources.redhat.com/pub/newlib/newlib-1.18.0.tar.gz || { exit 1; }
+wget --continue ftp://sources.redhat.com/pub/newlib/newlib-1.19.0.tar.gz || { exit 1; }
 
 ## Unpack the source code.
-rm -Rf newlib-1.18.0 && tar xfvz newlib-1.18.0.tar.gz && cd newlib-1.18.0 || { exit 1; }
+rm -Rf newlib-1.19.0 && tar xfvz newlib-1.19.0.tar.gz && cd newlib-1.19.0 || { exit 1; }
 
 ## Patch the source code.
-cat ../../patches/newlib-1.18.0-PS3.patch | patch -p1 || { exit 1; }
+cat ../../patches/newlib-1.19.0-PS3.patch | patch -p1 || { exit 1; }
 
 ## Create the build directory.
 mkdir build-spu && cd build-spu || { exit 1; }
