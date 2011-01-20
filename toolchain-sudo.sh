e24950a19b19469b04a2b0d7a8952be86aead0ba
Dan Peori <dan.peori@oopo.net>
2011-01-20 08:56:59 -0400

    Updated the toolchain-sudo.sh script with the new path locations.

--- toolchain-sudo.sh
+++ toolchain-sudo.sh
@@ -4,14 +4,15 @@
 ## Enter the ps3toolchain directory.
 cd "`dirname $0`" || { echo "ERROR: Could not enter the ps3toolchain directory."; exit 1; }
 
-## Set up the environment.
+## Set up the PS3DEV environment.
 export PS3DEV=/usr/local/ps3dev
-export PSL1GHT=$PS3DEV/psl1ght
-
-## Set up the path.
 export PATH=$PATH:$PS3DEV/bin
-export PATH=$PATH:$PS3DEV/ppu/bin
-export PATH=$PATH:$PS3DEV/spu/bin
+export PATH=$PATH:$PS3DEV/host/ppu/bin
+export PATH=$PATH:$PS3DEV/host/spu/bin
+
+## Set up the PSL1GHT environment.
+export PSL1GHT=$PS3DEV/psl1ght
+export PATH=$PATH:$PSL1GHT/host/bin
 
 ## Run the toolchain script.
 ./toolchain.sh $@ || { echo "ERROR: Could not run the toolchain script."; exit 1; }
