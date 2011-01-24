7b6241a850b7a60538cdcb270e5d38e1929672e9
Dan Peori <dan.peori@oopo.net>
2011-01-24 13:12:47 -0400

    Updated the check-bison script.

--- depends/check-bison.sh
+++ depends/check-bison.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-# check-bison.sh by Anisse Astier (????@???.???)
+# check-bison.sh by Dan Peori (dan.peori@oopo.net)
 
-if ! which yacc >/dev/null 2>&1
-then
-  command=bison
-else
-  command=yacc
-fi
-
-$command -V 1> /dev/null || { echo "ERROR: Install bison before continuing."; exit 1; }
+( bison -V || yacc -V ) 1> /dev/null 2> /dev/null || { echo "ERROR: Install bison before continuing."; exit 1; }
