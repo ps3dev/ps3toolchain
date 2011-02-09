83920da05d2ac77c30ca791d927ceb4a42c16b6b
Dan Peori <dan.peori@oopo.net>
2011-02-09 09:58:38 -0400

    Updated the ps3libraries link.

--- scripts/012-ps3libraries.sh
+++ scripts/012-ps3libraries.sh
@@ -2,7 +2,7 @@
 # ps3libraries.sh by Dan Peori (dan.peori@oopo.net)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/ooPo/PS3LIBRARIES/tarball/master -O ps3libraries.tar.gz || { exit 1; }
+wget --no-check-certificate https://github.com/ps3dev/ps3libraries/tarball/master -O ps3libraries.tar.gz || { exit 1; }
 
 ## Unpack the source code.
 rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries || { exit 1; }
