2ac653675e576da017ddbb2691139c32c40c95b5
miigotu <miigotu@hotmail.com>
2011-03-05 16:59:31 -0500

    Fix check scripts to find mingw installed libraries.

--- depends/check-ncurses.sh
+++ depends/check-ncurses.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # check-ncurses.sh by Dan Peori (dan.peori@oopo.net)
 
-( ls /usr/include/ncurses.h || ls /usr/include/ncurses/ncurses.h || ls /opt/local/include/ncurses.h ) 1> /dev/null 2> /dev/null || { echo "ERROR: Install ncurses before continuing."; exit 1; }
+( ls /usr/include/ncurses.h || ls /usr/include/ncurses/ncurses.h || ls /opt/local/include/ncurses.h  || ls $PS3DEV/include/curses.h ) 1> /dev/null 2> /dev/null || { echo "ERROR: Install ncurses before continuing."; exit 1; }
--- depends/check-ps3dev.sh
+++ depends/check-ps3dev.sh
@@ -11,10 +11,10 @@ if test ! $PS3DEV; then { echo "ERROR: Set \$PS3DEV before continuing."; exit 1;
 touch $PS3DEV/test.tmp 1> /dev/null || { echo "ERROR: Grant write permissions for $PS3DEV before continuing."; exit 1; }
 
 ## Check for $PS3DEV/bin in the path.
-echo $PATH | grep $PS3DEV/bin 1> /dev/null || { echo "ERROR: Add $PS3DEV/bin to your path before continuing."; exit 1; }
+( echo $PATH | grep $PS3DEV/bin || echo $PATH | grep %PS3DEV%/bin ) 1> /dev/null || { echo "ERROR: Add $PS3DEV/bin to your path before continuing."; exit 1; }
 
 ## Check for $PS3DEV/ppu/bin in the path.
-echo $PATH | grep $PS3DEV/host/ppu/bin 1> /dev/null || { echo "ERROR: Add $PS3DEV/host/ppu/bin to your path before continuing."; exit 1; }
+( echo $PATH | grep $PS3DEV/host/ppu/bin || echo $PATH | grep %PS3DEV%/host/ppu/bin ) 1> /dev/null || { echo "ERROR: Add $PS3DEV/host/ppu/bin to your path before continuing."; exit 1; }
 
 ## Check for $PS3DEV/spu/bin in the path.
-echo $PATH | grep $PS3DEV/host/spu/bin 1> /dev/null || { echo "ERROR: Add $PS3DEV/host/spu/bin to your path before continuing."; exit 1; }
+( echo $PATH | grep $PS3DEV/host/spu/bin || echo $PATH | grep %PS3DEV%/host/spu/bin ) 1> /dev/null || { echo "ERROR: Add $PS3DEV/host/spu/bin to your path before continuing."; exit 1; }
