c8b0d12f758d5e14a25580a3d7715078a77a9c18
unknown <Dustyn@.(none)>
2011-03-08 00:47:13 -0500

    Add a menu and info to the toolchain-mingw.sh script per ooPo request Detailed info about editting the profile and removing the changes included. oooh pretty colored/formatted text.

--- toolchain-mingw.sh
+++ toolchain-mingw.sh
@@ -15,9 +15,56 @@ MINGW32EXPORTS=\
 'export PATH="$PATH:$PSL1GHT/host/bin"')
 
 if [ $MSYSTEM == MINGW32 ]; then
+	DO_SET_EXPORTS=0
+	echo -e "\E[1;31mWARNING:\E[0m\E[1m If you continue, his script will modify your global profile!\E[0m"
+	echo
+	echo The purpose of this change is to add export paths to your profile.
+	echo To undo this, edit /etc/profile and removing these lines from the bottom:
+	echo -e '\E[4m(Note:\E[0m These are also the exports that will be added, take note.)'
+	echo
+	echo -e '\E[0;32m	export GITINSTALLDIR=/c/msysgit'
+	echo '	export PATH="$PATH:$GITINSTALLDIR/bin"'
+	echo '	export PATH="$PATH:$GITINSTALLDIR/mingw/bin"'
+	echo '	export PYINSTALLDIR=/c/python27'
+	echo '	export PATH="$PATH:$PYINSTALLDIR"'
+	echo '	export PS3DEV=/usr/local/ps3dev'
+	echo '	export PATH="$PATH:$PS3DEV/bin"'
+	echo '	export PATH="$PATH:$PS3DEV/host/ppu/bin"'
+	echo '	export PATH="$PATH:$PS3DEV/host/spu/bin"'
+	echo '	export PSL1GHT=$PS3DEV/psl1ght'
+	echo -e '	export PATH="$PATH:$PSL1GHT/host/bin"\E[0m'
+	echo
+	echo It is highly recommended that you do this, to have the same build that most 
+	echo others have, so that it is easier for others to help you when you have issues.
+	echo Also, the buildscripts will fail if certain paths and variables are not set.
+	echo
+	echo -e '\E[1mDo you want to set these exports?\E[0m'
+	echo
+
+	PS3="Enter 1, 2, or 3: "
+	options=("Yes, change my profile for me." "No, I have set them myself, but continue." "Quit, I will set them myself.")
+	select opt in "${options[@]}"
+	do
+		case $opt in
+		"Yes, change my profile for me.")
+			DO_SET_EXPORTS=1
+			break
+			;;
+		"No, I have set them myself, but continue.")
+			break
+			;;
+		"Quit, I will set them myself.")
+			echo -e "\E[1m Exitting, Run this script again when you are ready.\E[0m"
+			exit 0
+			;;
+		*) echo Invalid option, please try again.
+			;;
+		esac
+	done
+
 	NEWLINE=0
 	RESTART=0
-	for((i = 0; i < 11; i++)); do
+	for((i = 0; i < 11 && DO_SET_EXPORTS == 1; i++)); do
 		if ! grep -q "${MINGW32EXPORTS[${i}]}" /etc/profile; then
 			if [ $NEWLINE -ne 1 ]; then
 				echo >> /etc/profile ##In cases where no new line is at EOF.
@@ -28,7 +75,7 @@ if [ $MSYSTEM == MINGW32 ]; then
 		fi
 	done
 	if [ $RESTART -eq 1 ]; then
-		echo "Please restart mingw, or type '. /etc/profile' (without quotes)"
+		echo -e "Please restart mingw, or type \E[1m'. /etc/profile'\E[0m (without quotes)"
 		echo "and then run this script again."
 		exit 1
 	fi
@@ -46,7 +93,8 @@ if [ $MSYSTEM == MINGW32 ]; then
 	./configure --prefix="/mingw"
 	make
 	make install
-	
+	cd ..
+
 	## Install pkg-config and dependancy dll's
 	wget http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/pkg-config_0.25-1_win32.zip
 	wget http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/gettext-runtime_0.18.1.1-2_win32.zip
@@ -56,7 +104,7 @@ if [ $MSYSTEM == MINGW32 ]; then
 	unzip -o glib_2.26.1-1_win32.zip bin/*.dll -d /mingw > NUL
 	unzip -o gettext-runtime_0.18.1.1-2_win32.zip bin/*.dll -d /mingw > NUL
 
-	rm -Rf pkg-config_0.25-1_win32.zip glib_2.26.1-1_win32.zip gettext-runtime_0.18.1.1-2_win32.zip
+	rm -Rf pkg-config_0.25-1_win32.zip glib_2.26.1-1_win32.zip gettext-runtime_0.18.1.1-2_win32.zip libelf-0.8.13.tar.gz libelf-0.8.13
 
 	## Convert python.dll to a shared library
 	PYDLL="$WINDIR\SysWOW64\python27.dll"
@@ -67,6 +115,7 @@ if [ $MSYSTEM == MINGW32 ]; then
 	fi
 	dlltool --dllname "$PYDLL" --def python27.def --output-lib libpython27.a 
 	mv libpython27.a $PYINSTALLDIR/libs
+	rm -Rf python27.def
 
 	## Enter the ps3toolchain directory.
 	cd ..
