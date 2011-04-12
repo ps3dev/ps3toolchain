e197eaf10c65a838443eed6f2b7536a49188020c
Dan Peori <dan.peori@oopo.net>
2011-04-12 17:37:34 -0300

    Fixed the sudo script.

--- toolchain-sudo.sh
+++ toolchain-sudo.sh
@@ -4,15 +4,14 @@
 ## Enter the ps3toolchain directory.
 cd "`dirname $0`" || { echo "ERROR: Could not enter the ps3toolchain directory."; exit 1; }
 
-## Set up the PS3DEV environment.
+## Set up the environment.
 export PS3DEV=/usr/local/ps3dev
-export PATH=$PATH:$PS3DEV/bin
-export PATH=$PATH:$PS3DEV/host/ppu/bin
-export PATH=$PATH:$PS3DEV/host/spu/bin
-
-## Set up the PSL1GHT environment.
 export PSL1GHT=$PS3DEV/psl1ght
-export PATH=$PATH:$PSL1GHT/host/bin
+
+## Set up the path.
+export PATH=$PATH:$PS3DEV/bin
+export PATH=$PATH:$PS3DEV/ppu/bin
+export PATH=$PATH:$PS3DEV/spu/bin
 
 ## Run the toolchain script.
 ./toolchain.sh $@ || { echo "ERROR: Could not run the toolchain script."; exit 1; }
