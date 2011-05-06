9dfe12bf0ccf93c51d13dc2a9fcbcd35de762ccd
Marcus Comstedt <marcus@mc.pp.se>
2011-05-06 19:54:13 +0200

    Fix broken check for python header files
    Using [ -f /bla/bla/*/bla ] does not work since it can expand to
    multiple arguments and -f only accepts one.  Use ls to test for
    existence instead.

--- depends/check-python.sh
+++ depends/check-python.sh
@@ -9,4 +9,4 @@ pyprefix=$(python-config --prefix)
 [ $? -eq 0 ] || { echo "ERROR: Install python-dev before continuing."; exit 1; }
 
 ## Check for python header files
-[ -f "${pyprefix}"/include/python2.*/Python.h -o -f /opt/local/include/python2.*/Python.h -o -f $PYINSTALLDIR/include/Python.h ] || { echo "ERROR: Install python-dev before continuing."; exit 1; }
+( ls -1d "${pyprefix}"/include/python2.*/Python.h || ls -1d /opt/local/include/python2.*/Python.h ) 1> /dev/null 2> /dev/null || [ -f "$PYINSTALLDIR/include/Python.h" ] || { echo "ERROR: Install python-dev before continuing."; exit 1; }
