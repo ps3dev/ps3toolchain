e1503c4c54fec601bc07a1d810f9da98ef404fd2
Dan Peori <dan.peori@oopo.net>
2011-05-25 11:18:08 -0300

    Added another symlink.

--- scripts/006-symlinks-PPU.sh
+++ scripts/006-symlinks-PPU.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # symlinks-PPU.sh by Dan Peori (dan.peori@oopo.net)
 
-## Enter the PPU bin directory.
-cd $PS3DEV/ppu/bin
+## Enter the PPU directory.
+cd $PS3DEV/ppu
 
-## Create the symlinks.
+## Create the directory symlinks.
+ln -s powerpc64-ps3-lv2 ppu
+
+## Enter the bin directory.
+cd bin
+
+## Create the bin symlinks.
 ls powerpc64-ps3-lv2-* | cut -c19- | awk '{ print "ln -s powerpc64-ps3-lv2-"$0" ppu-"$0 }' | sh
