f87f4471800f574a77b05e210aa001c2f8cd444f
Marcus Comstedt <marcus@mc.pp.se>
2011-07-05 18:57:42 +0200

    Upgrade binutils to 2.21.1
    Binutils 2.21.1 has been released, and contains all the tbss fixes we
    used to backport, as well as better handling of multi-TOC programs
    (.got larger than 64K).

--- scripts/001-binutils-2.21.1-PPU.sh
+++ scripts/001-binutils-2.21.1-PPU.sh
@@ -1,28 +1,28 @@
 #!/bin/bash -e
-# binutils-2.20.1-PPU.sh by Dan Peori (dan.peori@oopo.net)
+# binutils-2.21.1-PPU.sh by Dan Peori (dan.peori@oopo.net)
 
-if [ ! -d binutils-2.20.1 ]; then
+if [ ! -d binutils-2.21.1 ]; then
 
   ## Download the source code.
-  wget --continue ftp://ftp.gnu.org/gnu/binutils/binutils-2.20.1.tar.bz2
+  wget --continue ftp://ftp.gnu.org/gnu/binutils/binutils-2.21.1.tar.bz2
 
   ## Unpack the source code.
-  tar xfvj binutils-2.20.1.tar.bz2
+  tar xfvj binutils-2.21.1.tar.bz2
 
   ## Patch the source code.
-  cat ../patches/binutils-2.20.1-PS3.patch | patch -p1 -d binutils-2.20.1
+  cat ../patches/binutils-2.21.1-PS3.patch | patch -p1 -d binutils-2.21.1
 
 fi
 
-if [ ! -d binutils-2.20.1/build-ppu ]; then
+if [ ! -d binutils-2.21.1/build-ppu ]; then
 
   ## Create the build directory.
-  mkdir binutils-2.20.1/build-ppu
+  mkdir binutils-2.21.1/build-ppu
 
 fi
 
 ## Enter the build directory.
-cd binutils-2.20.1/build-ppu
+cd binutils-2.21.1/build-ppu
 
 ## Configure the build.
 ../configure --prefix="$PS3DEV/ppu" --target="powerpc64-ps3-elf" \
--- scripts/007-binutils-2.21.1-SPU.sh
+++ scripts/007-binutils-2.21.1-SPU.sh
@@ -1,28 +1,28 @@
 #!/bin/bash -e
-# binutils-2.20.1-SPU.sh by Dan Peori (dan.peori@oopo.net)
+# binutils-2.21.1-SPU.sh by Dan Peori (dan.peori@oopo.net)
 
-if [ ! -d binutils-2.20.1 ]; then
+if [ ! -d binutils-2.21.1 ]; then
 
   ## Download the source code.
-  wget --continue ftp://ftp.gnu.org/gnu/binutils/binutils-2.20.1.tar.bz2
+  wget --continue ftp://ftp.gnu.org/gnu/binutils/binutils-2.21.1.tar.bz2
 
   ## Unpack the source code.
-  tar xfvj binutils-2.20.1.tar.bz2
+  tar xfvj binutils-2.21.1.tar.bz2
 
   ## Patch the source code.
-  cat ../patches/binutils-2.20.1-PS3.patch | patch -p1 -d binutils-2.20.1
+  cat ../patches/binutils-2.21.1-PS3.patch | patch -p1 -d binutils-2.21.1
 
 fi
 
-if [ ! -d binutils-2.20.1/build-spu ]; then
+if [ ! -d binutils-2.21.1/build-spu ]; then
 
   ## Create the build directory.
-  mkdir binutils-2.20.1/build-spu
+  mkdir binutils-2.21.1/build-spu
 
 fi
 
 ## Enter the build directory.
-cd binutils-2.20.1/build-spu
+cd binutils-2.21.1/build-spu
 
 ## Configure the build.
 ../configure --prefix="$PS3DEV/spu" --target="spu" \
