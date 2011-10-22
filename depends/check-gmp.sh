152567794df8e8f4d136fe245879cd581ed9d485
Marcus Comstedt <marcus@mc.pp.se>
2011-10-22 13:41:35 +0200

    Add OpenCSW path to gmp.h, and replace unportable -e flag to [.

--- depends/check-gmp.sh
+++ depends/check-gmp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # check-gmp.sh by Timothy Redaelli (timothy@redaelli.eu)
 
-[ -e /usr/include/gmp.h -o -e /opt/local/include/gmp.h -o -e /usr/local/include/gmp.h ] || { echo "ERROR: Install gmp before continuing."; exit 1; }
+[ -f /usr/include/gmp.h -o -f /opt/local/include/gmp.h -o -f /usr/local/include/gmp.h -o -f /opt/csw/include/gmp.h ] || { echo "ERROR: Install gmp before continuing."; exit 1; }
