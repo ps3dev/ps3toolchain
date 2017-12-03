71e9c0222ca4f0d3041a45b6821c05f390b27fa3
Marcus Comstedt <marcus@mc.pp.se>
2017-12-03 01:42:45 +0100

    Use up-to-date config.guess and config.sub
    This is required to build on modern hosts.

--- scripts/003-gdb-PPU.sh
+++ scripts/003-gdb-PPU.sh
@@ -8,8 +8,15 @@ if [ ! -d ${GDB} ]; then
   ## Download the source code.
   if [ ! -f ${GDB}.tar.bz2 ]; then wget --continue https://ftp.gnu.org/gnu/gdb/${GDB}.tar.bz2; fi
 
+  ## Download an up-to-date config.guess and config.sub
+  if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
+  if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
+
   ## Unpack the source code.
   tar xfvj ${GDB}.tar.bz2
+
+  ## Replace config.guess and config.sub
+  cp config.guess config.sub ${GDB}
 
 fi
 
