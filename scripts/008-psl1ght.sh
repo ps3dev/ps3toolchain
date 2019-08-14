27d22c017c2425d44421068bc1227b6b108368a5
Damian Parrino <bucanero@users.noreply.github.com>
2019-08-13 22:30:01 -0300

    Update 008-psl1ght.sh

--- scripts/008-psl1ght.sh
+++ scripts/008-psl1ght.sh
@@ -2,7 +2,7 @@
 # psl1ght.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/ps3dev/PSL1GHT/tarball/master -O psl1ght.tar.gz
+wget --no-check-certificate https://github.com/bucanero/PSL1GHT/tarball/master -O psl1ght.tar.gz
 
 ## Unpack the source code.
 rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz
