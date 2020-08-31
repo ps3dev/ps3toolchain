7f060b7bec3f34ee4dc95e632d646a017a935623
Damián Parrino <bucanero@users.noreply.github.com>
2020-08-31 11:00:26 -0300

    Update 009-ps3libraries.sh

--- scripts/009-ps3libraries.sh
+++ scripts/009-ps3libraries.sh
@@ -2,7 +2,7 @@
 # ps3libraries.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/ps3dev/ps3libraries/tarball/master -O ps3libraries.tar.gz
+wget --no-check-certificate https://github.com/bucanero/ps3libraries/tarball/master -O ps3libraries.tar.gz
 
 ## Unpack the source code.
 rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries
