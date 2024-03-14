21a85b41fdc933f22939de356ddc8feff856d08c
Humberto Dias <hldtux@gmail.com>
2024-03-14 18:28:57 -0300

    Update 009-ps3libraries.sh

--- scripts/009-ps3libraries.sh
+++ scripts/009-ps3libraries.sh
@@ -2,7 +2,7 @@
 # ps3libraries.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/humbertodias/ps3libraries/tarball/master -O ps3libraries.tar.gz
+wget --no-check-certificate https://github.com/humbertodias/ps3libraries/tarball/psl1ght-2.30.1 -O ps3libraries.tar.gz
 
 ## Unpack the source code.
 rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries
