899711443c6054e407c10d9d07cb1ae5738dce7f
Humberto Dias <hldtux@gmail.com>
2024-04-06 09:38:09 -0300

    download source code only if directory doesn't exist

--- scripts/008-psl1ght.sh
+++ scripts/008-psl1ght.sh
@@ -1,14 +1,19 @@
 #!/bin/sh -e
 # psl1ght.sh by Naomi Peori (naomi@peori.ca)
 
-## Download the source code.
-wget --no-check-certificate https://github.com/ps3dev/PSL1GHT/tarball/master -O psl1ght.tar.gz
+PSL1GHT="PSL1GHT"
 
-## Unpack the source code.
-rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz
+if [ ! -d ${PSL1GHT} ]; then
 
-## Create the build directory.
-cd psl1ght
+    ## Download the source code.
+    wget --no-check-certificate https://github.com/ps3dev/$PSL1GHT/tarball/master -O $PSL1GHT.tar.gz
+
+    ## Unpack the source code.
+    rm -Rf $PSL1GHT && mkdir $PSL1GHT && tar --strip-components=1 --directory=$PSL1GHT -xvzf $PSL1GHT.tar.gz
+
+fi
+
+cd $PSL1GHT
 
 ## Compile and install.
 ${MAKE:-make} install-ctrl && ${MAKE:-make} && ${MAKE:-make} install
--- scripts/009-ps3libraries.sh
+++ scripts/009-ps3libraries.sh
@@ -1,11 +1,19 @@
 #!/bin/sh -e
 # ps3libraries.sh by Naomi Peori (naomi@peori.ca)
 
-## Download the source code.
-wget --no-check-certificate https://github.com/humbertodias/ps3libraries/tarball/psl1ght-2.30.1 -O ps3libraries.tar.gz
+PS3LIBRARIES="ps3libraries"
 
-## Unpack the source code.
-rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries
+if [ ! -d ${PSL1GHT} ]; then
+
+    ## Download the source code.
+    wget --no-check-certificate https://github.com/humbertodias/$PS3LIBRARIES/tarball/psl1ght-2.30.1 -O $PS3LIBRARIES.tar.gz
+
+    ## Unpack the source code.
+    rm -Rf $PS3LIBRARIES && mkdir $PS3LIBRARIES && tar --strip-components=1 --directory=$PS3LIBRARIES -xvzf $PS3LIBRARIES.tar.gz
+
+fi
+
+cd $PS3LIBRARIES
 
 ## Compile and install.
 ./libraries.sh
