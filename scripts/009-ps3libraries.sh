08351b75a873dbaa7ba638f59b2b0b9ec23bc255
Humberto Dias <hldtux@gmail.com>
2024-04-06 13:48:49 -0300

    typo

--- scripts/009-ps3libraries.sh
+++ scripts/009-ps3libraries.sh
@@ -3,7 +3,7 @@
 
 PS3LIBRARIES="ps3libraries"
 
-if [ ! -d ${PSL1GHT} ]; then
+if [ ! -d ${PS3LIBRARIES} ]; then
 
     ## Download the source code.
     wget --no-check-certificate https://github.com/humbertodias/$PS3LIBRARIES/tarball/psl1ght-2.30.1 -O $PS3LIBRARIES.tar.gz
