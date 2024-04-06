c450e5352b518576da980b8f9fea019fbda9ccc9
Humberto Dias <hldtux@gmail.com>
2024-04-06 11:03:44 -0300

    typo

--- scripts/008-psl1ght.sh
+++ scripts/008-psl1ght.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -e
 # psl1ght.sh by Naomi Peori (naomi@peori.ca)
 
-PSL1GHT="PSL1GHT"
+PSL1GHT_DIR="PSL1GHT"
 
-if [ ! -d ${PSL1GHT} ]; then
+if [ ! -d ${PSL1GHT_DIR} ]; then
 
     ## Download the source code.
-    wget --no-check-certificate https://github.com/ps3dev/$PSL1GHT/tarball/master -O $PSL1GHT.tar.gz
+    wget --no-check-certificate https://github.com/ps3dev/$PSL1GHT_DIR/tarball/master -O $PSL1GHT_DIR.tar.gz
 
     ## Unpack the source code.
-    rm -Rf $PSL1GHT && mkdir $PSL1GHT && tar --strip-components=1 --directory=$PSL1GHT -xvzf $PSL1GHT.tar.gz
+    rm -Rf $PSL1GHT_DIR && mkdir $PSL1GHT_DIR && tar --strip-components=1 --directory=$PSL1GHT_DIR -xvzf $PSL1GHT_DIR.tar.gz
 
 fi
 
-cd $PSL1GHT
+cd $PSL1GHT_DIR
 
 ## Compile and install.
 ${MAKE:-make} install-ctrl && ${MAKE:-make} && ${MAKE:-make} install
