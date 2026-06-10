ef0d19d7981e47806ba604f8c086b8ee8c34e196
Damian Parrino <bucanero@users.noreply.github.com>
2026-06-10 10:39:59 -0300

    build toolchain only
    if env var is set,
    - skip psl1ght
    - skip ps3libraries

--- scripts/008-psl1ght.sh
+++ scripts/008-psl1ght.sh
@@ -4,6 +4,12 @@
 ## Download the source code.
 wget --no-check-certificate https://github.com/ps3dev/PSL1GHT/tarball/master -O psl1ght.tar.gz
 
+## Check if we want to skip this step
+if [[ -n "$BUILD_PS3TOOLCHAIN_ONLY" ]]; then
+    echo "PS3 Toolchain only set. Skipping..."
+    exit 0
+fi
+
 ## Unpack the source code.
 rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz
 
--- scripts/009-ps3libraries.sh
+++ scripts/009-ps3libraries.sh
@@ -4,6 +4,12 @@
 ## Download the source code.
 wget --no-check-certificate https://github.com/ps3dev/ps3libraries/tarball/master -O ps3libraries.tar.gz
 
+## Check if we want to skip this step
+if [[ -n "$BUILD_PS3TOOLCHAIN_ONLY" ]]; then
+    echo "PS3 Toolchain only set. Skipping..."
+    exit 0
+fi
+
 ## Unpack the source code.
 rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries
 
